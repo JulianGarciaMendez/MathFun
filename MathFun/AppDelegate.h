a8fe9afb5e403cb948bf59789c7971df1513ad65
Chen Chun-Da Jimmy <chundachen1230@172-26-50-58.dynapool.nyu.edu>
2012-06-19 18:31:49 -0400

    Initial Commit

---
+++ MathFun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MathFun
+//
+//  Created by Chun-Da Jimmy Chen on 12/6/19.
+//  Copyright (c) 2012年 chundachen1230@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MathFun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MathFun
+//
+//  Created by Chun-Da Jimmy Chen on 12/6/19.
+//  Copyright (c) 2012年 chundachen1230@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
